427349aa4b58e91870666f58d40f437e0d99fd10
Florian <florian.tellier02@gmail.com>
2021-03-20 15:33:08 +0100

    premier commit / debut blog2

---
+++ src/components/Blog2Roll.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql, StaticQuery } from 'gatsby'
+import PreviewCompatibleImage from './PreviewCompatibleImage'
+
+class TestBlog extends React.Component {
+    render() {
+        const { data } = this.props
+        const { edges: posts } = data.allMarkdownRemark
+
+        return (
+            <div className="columns is-multiline">
+                {posts &&
+                    posts.map(({ node: post }) => (
+                        <div className="is-parent column is-6" key={post.id}>
+
+                            {post.fields.slug.indexOf("blog2") > 0 ? (
+                                <article
+                                    className={`blog-list-item tile is-child box notification ${post.frontmatter.featuredpost ? 'is-featured' : ''
+                                        }`}
+                                >
+                                    <header>
+                                        <h1>{post.frontmatter.title}{post.fields.slug}</h1>
+                                        <p className="post-meta">
+                                            <span className="subtitle is-size-5 is-block">
+                                                {post.frontmatter.date}
+                                            </span>
+                                        </p>
+                                    </header>
+                                    <p>
+                                        {post.excerpt}
+                                        <br />
+                                        <br />
+                                        {post.fields.slug == "blog2" ? "oui" : "non"}
+                                        {post.fields.slug.indexOf("Blog2") > 0 ? "oui" : "non"}
+                                    </p>
+                                    <p>
+                                        {post.fields.slug == "Blog2" ? console.log("oui") : console.log("non")}
+                                    </p>
+                                </article>
+
+                            ) : null}
+
+
+
+                        </div>
+                    ))}
+            </div>
+        )
+    }
+}
+
+TestBlog.propTypes = {
+    data: PropTypes.shape({
+        allMarkdownRemark: PropTypes.shape({
+            edges: PropTypes.array,
+        }),
+    }),
+}
+
+export default () => (
+    <StaticQuery
+        query={graphql`
+      query TestBlogQuery {
+        allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+          filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+        ) {
+          edges {
+            node {
+              excerpt(pruneLength: 400)
+              id
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+                templateKey
+                date(formatString: "MMMM DD, YYYY")
+                featuredpost
+                featuredimage {
+                  childImageSharp {
+                    fluid(maxWidth: 120, quality: 100) {
+                      ...GatsbyImageSharpFluid
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+        render={(data, count) => <TestBlog data={data} count={count} />}
+    />
+)
